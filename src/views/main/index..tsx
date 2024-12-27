9a60f0bd9975af7c92d6c055e53990d8fc2eb4ce
paula borges <paulabzambeli@gmail.com>
2024-12-27 10:15:27 -0300

    first commit

---
+++ src/views/main/index..tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function Main() {
+  return (
+    <div className="container-main">
+      <h1>Helloo</h1>
+    </div>
+  );
+}
+
+export default Main;
